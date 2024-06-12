6b569a09bc17501c3554f4a7488e59261ac77f36
DominikSubocz <dominiksubocz@gmail.com>
2024-06-12 13:11:48 +0100

    Responsiveness & Themes
    I have adjusted responsiveness of different elements and added JS code for switching themes.
    
    I will work on getting the mobile menu to show up and final touches when I come back.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 // Add some interactivity to the website
 console.log("Hello, world!");
+let element = document.getElementById("checkbox");
+
+
+
+
+
+element.addEventListener("change", function(){
+    let h1 = document.querySelector("h1");
+    let logo = document.getElementById("logo");
+
+    let lightImageUrl = "images/alarado-icon-homepage.svg"
+    let darkImageUrl = "images/alarado-icon-homepage-dark.svg"
+
+    if (element.checked == true) {
+        console.log("Light");
+        document.body.setAttribute("data-theme", "light");
+        logo.src = lightImageUrl;
+      } else {
+        console.log("Dark");
+        document.body.setAttribute("data-theme", "dark");
+        logo.src = darkImageUrl;
+
+      }
+});
+
